740f56e93059b53c9315da321b12978de3a16262
johnnyeven_work <johnnyeven@gmail.com>
2014-04-14 17:13:38 +0800

    1

--- service/application/controllers/notify/role.php
+++ service/application/controllers/notify/role.php
@@ -5,7 +5,7 @@ class Role extends CI_Controller
 {
 	/**
 	 * 
-	 * 用户平台帐户相关操作
+	 * 用户平台角色相关操作
 	 * 
 	 * @author johnnyEven
 	 * @version Pulse/service account.php - 1.0.1.20130409 10:52
@@ -17,6 +17,11 @@ class Role extends CI_Controller
 		parent::__construct();
 	}
 	
+	/**
+	 *
+	 * 角色进入游戏时调用
+	 *
+	 */
 	public function enter($format = 'json')
 	{
 		$account_id = $this->input->post('account_id');
@@ -53,6 +58,11 @@ class Role extends CI_Controller
 		}
 	}
 
+	/**
+	 *
+	 * 角色创建时调用
+	 *
+	 */
 	public function create($format = 'json')
 	{
 		$account_id = $this->input->post('account_id');
@@ -86,6 +96,39 @@ class Role extends CI_Controller
 				'code'	=>	-1
 			);
 			echo $this->return_format->format($json);
+		}
+	}
+
+	/**
+	 *
+	 * 角色同步时调用
+	 *
+	 */
+	public function sync($format = 'json')
+	{
+		$product_id = $this->input->post('product_id');
+		$server_id = $this->input->post('server_id');
+		$partner = $this->input->post('partner');
+		$account_id = $this->input->post('account_id');
+		$role_id = $this->input->post('role_id');
+		$level = $this->input->post('level');
+		$last_mission = $this->input->post('mission');
+
+		if(!empty($product_id) && !empty($server_id) && !empty($account_id) && !empty($role_id))
+		{
+			$partner = empty($partner) ? 0 : intval($partner);
+
+			$key = array(
+				'product_id'		=>	$product_id,
+				'server_id'			=>	$server_id,
+				'account_id'		=>	$account_id,
+				'role_id'			=>	$role_id,
+				'partner'			=>	$partner
+			);
+			$parameter = array(
+				'level'			=>	$level,
+				'last_mission'	=>	$last_mission
+			);
 		}
 	}
 }
